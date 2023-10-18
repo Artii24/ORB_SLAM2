29e477c04d87ce0e8e2f38b692182d599f5a4227
Artii_Egorov <36917759+Artii24@users.noreply.github.com>
2023-10-18 20:12:42 +0600

    Remove MapPub from System.cc

--- src/System.cc
+++ src/System.cc
@@ -257,11 +257,6 @@ cv::Mat System::TrackMonocular(const cv::Mat &im, const double &timestamp)
     mTrackedMapPoints = mpTracker->mCurrentFrame.mvpMapPoints;
     mTrackedKeyPointsUn = mpTracker->mCurrentFrame.mvKeysUn;
 
-    //if (Tcw.rows != 0){
-    //    MapPub->SetCurrentCameraPose(Tcw);
-    //}
-
-    //MapPub->Refresh();
 
     return Tcw;
 }
